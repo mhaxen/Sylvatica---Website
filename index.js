dd402ae84a5ef9cb35a50bdf00071a275ceb56e3
mhaxen <m.haxen@gmail.com>
2021-11-21 00:21:30 +0100

    Made radical changes huehue

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // title events
 
-$(".home").mouseenter(function(){
+$(".home").mouseenter(
+  function(){
   $(".home").animate({
     width: "100%"
   });
-})
+});
 
-$(".home").mouseleave(function(){
+$(".home").mouseleave(
+  function(){
   $(".home").animate({
     width: "80%"
   });
-})
+});
+
+$(".navtitle").click(
+  function(){
+  $(this).css("color", "red");
+  setTimeout(function () {
+    $(".navtitle").css("color", "white");
+  }, 10);
+
+  $("#" + this.parentNode.id + " .togglable-section").slideToggle("slow");
+});
