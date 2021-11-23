92255af52c463d3d3754357b0ae77efefb0bc87d
mhaxen <m.haxen@gmail.com>
2021-11-23 13:51:33 +0100

    fix

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 //imports
+const express = require("express");
+
+
+
+//application behavior
+app = express()
+app.use(express.static(__dirname + "/css/styles.css"))
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(){
+  console.log("Server is up and listening on port 3000");
+});
 
 
 //mouse behavior
-$(".home").mouseenter(
-  function(){
-  $(".home").animate({
-    width: "100%"
-  });
-});
-
-$(".home").mouseleave(
-  function(){
-  $(".home").animate({
-    width: "80%"
-  });
-});
-
-$(".navtitle").click(
-  function(){
-  $(this).css("color", "red");
-  setTimeout(function () {
-    $(".navtitle").css("color", "white");
-  }, 10);
-
-  $("#" + this.parentNode.id + " .togglable-section").slideToggle("slow");
-});
+// $(".home").mouseenter(
+//   function(){
+//   $(".home").animate({
+//     width: "100%"
+//   });
+// });
+//
+// $(".home").mouseleave(
+//   function(){
+//   $(".home").animate({
+//     width: "80%"
+//   });
+// });
+//
+// $(".navtitle").click(
+//   function(){
+//   $(this).css("color", "red");
+//   setTimeout(function () {
+//     $(".navtitle").css("color", "white");
+//   }, 10);
+//
+//   $("#" + this.parentNode.id + " .togglable-section").slideToggle("slow");
+// });
